15597bc04f32a3b497dfc974ff390216f8aa8d69
Ray <q8977452@gmail.com>
2021-02-27 19:35:52 +0800

    feat(ORM): 增加Product

--- flask/main.py
+++ flask/main.py
@@ -1,6 +1,46 @@
-from app import create_app
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = create_app('development')
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user_name:password@IP:5432/db_name"
+app.config['SQLALCHEMY_RECORD_QUERIES'] = True
+
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    pid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(
+        db.String(30), unique=True, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    img = db.Column(
+        db.String(100), unique=True, nullable=False)
+    description = db.Column(
+        db.String(255), nullable=False)
+    state = db.Column(
+        db.String(10), nullable=False)
+    insert_time = db.Column(db.DateTime, default=datetime.now)
+    update_time = db.Column(
+        db.DateTime, onupdate=datetime.now, default=datetime.now)
+
+
+    def __init__(self, name, price, img, description, state):
+        self.name = name
+        self.price = price
+        self.img = img
+        self.description = description
+        self.state = state
+@app.route('/')
+def index():
+    # Create data
+    db.create_all()
+
+    return 'ok'
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
