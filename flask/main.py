db6cd54a143b9b149f3b85a8bd90481a2149fc18
Ray <q8977452@gmail.com>
2021-02-27 19:37:23 +0800

    feat(ORM): 增加User、AddToCar

--- flask/main.py
+++ flask/main.py
@@ -34,6 +34,43 @@ class Product(db.Model):
         self.img = img
         self.description = description
         self.state = state
+
+class User(db.Model):
+    __tablename__ = 'user'
+    uid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+    insert_time = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_time = db.Column(
+        db.DateTime, onupdate=datetime.now, default=datetime.now, nullable=False)
+
+    db_user_atc = db.relationship("AddToCar", backref="user")
+
+    def __init__(self, name, password, role):
+        self.name = name
+        self.password = password
+        self.role = role
+
+class AddToCar(db.Model):
+    __tablename__ = 'addtocar'
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    state = db.Column(db.String(5), nullable=False)
+    insert_time = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    update_time = db.Column(
+        db.DateTime, onupdate=datetime.now, default=datetime.now, nullable=False)
+
+    uid = db.Column(db.Integer, db.ForeignKey('user.uid'), nullable=False)
+    pid = db.Column(db.Integer, db.ForeignKey('product.pid'), nullable=False)
+
+    def __init__(self, uid, pid, quantity, state):
+        self.uid = uid
+        self.pid = pid
+        self.quantity = quantity
+        self.state = state
+
+
 @app.route('/')
 def index():
     # Create data
